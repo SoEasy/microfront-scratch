157405004c048eaa25374df5941f5b8a10adea54
vhaldemario <sannikov_v@tochka.com>
2023-10-03 11:42:33 +0300

    prepare configs

---
+++ config/build-mf.js
@@ -0,0 +1,62 @@
+const {resolve} = require('path');
+
+function resolvePath(...args) {
+  return resolve(process.cwd(), ...args)
+}
+
+function generateConfig(mfName, esm = false) {
+  const projectPath = resolvePath(mfName);
+
+  return {
+    mode: 'production',
+    entry: {
+      index: resolvePath(mfName, 'index.jsx'),
+    },
+    output: {
+      clean: true,
+      path: resolvePath('public', mfName),
+      filename: esm ? '[name].esm.js' : '[name].assign.js',
+      library: esm ? {
+        type: 'module'
+      } : {
+        type: 'assign',
+        name: `window.ourMfExports["${mfName}"]`
+      }
+    },
+    experiments: esm ?{
+      outputModule: true
+    } : {},
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: ['node_modules', projectPath]
+    },
+    optimization: {
+      minimize: false,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: projectPath,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-react',
+                    {
+                      runtime: 'automatic'
+                    }
+                  ]
+                ]
+              }
+            }
+          ]
+        },
+      ]
+    }
+  };
+}
+
+module.exports = { generateConfig };
--- config/sample.config.js
+++ config/sample.config.js
@@ -1,61 +1,6 @@
-const {resolve} = require('path');
-
-function resolvePath(...args) {
-  return resolve(process.cwd(), ...args)
-}
-
-function generateConfig(dirName) {
-  const projectPath = resolvePath(dirName);
-
-  return {
-    mode: 'production',
-    entry: {
-      index: resolvePath(dirName, 'index.jsx'),
-    },
-    output: {
-      clean: true,
-      path: resolvePath('public', dirName),
-      filename: '[name].js',
-      library: {
-        type: 'module'
-      }
-    },
-    experiments: {
-      outputModule: true
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-      modules: ['node_modules', projectPath]
-    },
-    optimization: {
-      minimize: false,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: projectPath,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: [
-                  [
-                    '@babel/preset-react',
-                    {
-                      runtime: 'automatic'
-                    }
-                  ]
-                ]
-              }
-            }
-          ]
-        },
-      ]
-    }
-  };
-}
+const { generateConfig } = require('./build-mf');
 
 module.exports = [
-  generateConfig('sample'),
-];
+  generateConfig('sample', true),
+  generateConfig('sample', false),
+]
